4b614dac729aef137ca6c8b09327f3cacb61a1af
Nate Smith <nate@developmentseed.org>
2014-01-09 18:53:03 -0500

    changing rxnorm to search by NDC code once

--- bin/rxnorm.py
+++ bin/rxnorm.py
@@ -5,32 +5,21 @@ import simplejson as json
 
 # Uses setid and product_name from SPL
 
-def rxNorm(splSETID, name):
+def rxNorm(ndc):
 	baseurl = 'http://rxnav.nlm.nih.gov/REST/'
 	
+	#http://rxnav.nlm.nih.gov/REST/rxcui?idtype=NDC&id=0591-2234-10
 	# Set url parameters for searching RXNorm for SETID
-	setidSearch = 'rxcui?idtype=SPL_SET_ID&id='
-	searchOption = '&allsrc=value'
-	
-	# Set url parameters to matching name with RXNorm 
-	check = 'rxcui/'
-	checkOption = '/allProperties?prop=NAMES'
+	ndcSearch = 'rxcui?idtype=NDC&id='
 	
 	# Request RXNorm API to return json 
 	header = {'Accept': 'application/json'}
 
-	r = requests.get(baseurl+setidSearch+splSETID+searchOption, headers=header)
+	# Search RXNorm using NDC code, return RXCUI id
+	r = requests.get(baseurl+ndcSearch+ndc, headers=header)
+	rxID = json.loads(r.text, encoding="utf-8")
+	rxCUI = rxID['idGroup']['rxnormId'][0]
 
-	rxIDs = json.loads(r.text, encoding="utf-8")
+	return rxCUI
 
-	for i in rxIDs['idGroup']['rxnormId']:
-		rCheck = requests.get(baseurl+check+i+checkOption, headers=header)
-		checkName = json.loads(rCheck.text, encoding="utf-8")
-		if checkName['propConceptGroup']['propConcept'][0]['propValue'].lower() == name.lower():
-			return i
-		# need to add rxtty and rxname 
-
-# Sample data from SPL: 
-# 	splSETID = 'db1311ad-9732-4fdb-80dc-95b09077e3d0'
-# 	name = 'TRIAZOLAM'
-rxNorm('0a4d39b0-396f-4ae0-9332-68557c6d4f74', 'Nabumetone')
+rxNorm('0591-2234-10')
